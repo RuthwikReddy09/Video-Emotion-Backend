71abaee0023f129d0feed852c7400f3ac996b275
ruthwikreddy09 <druthwik09@gmail.com>
2024-02-07 15:10:31 +0530

    video streamed to frontend

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, Response
+import cv2
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from time import sleep
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing import image
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)  
+face_classifier = cv2.CascadeClassifier("Live-Streaming-using-OpenCV-Flask\haarcascade_frontalface_default.xml")
+classifier =load_model("Live-Streaming-using-OpenCV-Flask\model.h5")
+emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
+cap = cv2.VideoCapture(0)
+
+
+def gen_frames():  # generate frame by frame from camera
+    while True:
+        _, frame = cap.read()
+        labels = []
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(gray)
+
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+            roi_gray = gray[y:y+h,x:x+w]
+            roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+
+            if np.sum([roi_gray])!=0:
+                roi = roi_gray.astype('float')/255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi,axis=0)
+                prediction = classifier.predict(roi)[0]
+                label=emotion_labels[prediction.argmax()]
+                label_position = (x,y)
+                cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+            else:
+                cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+        # cv2.imshow('Emotion Detector',frame)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
+    # cap.release()
+    # cv2.destroyAllWindows()
+
+@app.route('/video_feed')
+def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
